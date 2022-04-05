91c7779a7f2f8d8ff1ba6bf5d9a6a3b93ae6a888
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-04-05 08:11:07 +0530

    Update insta_cmds.py

--- plugins/insta_cmds.py
+++ plugins/insta_cmds.py
@@ -1,8 +1,11 @@
+import instaloader 
+import shutil
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters
 from config import Config
-from instaloader import Profile
+from instaloader import Profile 
+from instaloader.exceptions import QueryReturnedNotFoundException, ProfileNotExistsException
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong
 import os
 from utils import *
@@ -235,3 +238,29 @@ async def highlights(bot, message):
     await download_insta(command, m, dir)
     await upload(m, bot, chat_id, dir)
 
+@Client.on_message(filters.private & filters.command(["profile_pic", "dp"]))
+async def dp(_, msg):
+    status = await msg.reply('ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ....', quote=True)
+    if len(msg.command) == 1:
+        await msg.reply("ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ ᴜsᴇ ᴇᴍᴘᴛʏ ᴄᴏᴍᴍᴀɴᴅ. ʙᴇʟᴏᴡ ɪs ᴛʜᴇ ʀɪɢʜᴛ ғᴏʀᴍᴀᴛ ᴛᴏ ɢᴇᴛ ᴀ ᴘʀᴏғɪʟᴇ ᴘɪᴄ. "
+                        "\n\n`/profile_pic instagram-username` \n\nExample : `/profile_pic taaarannn`")
+        return
+    elif len(msg.command) > 2:
+        await msg.reply("Use 1 username at a time.")
+        return
+    text = msg.command[1]
+    if text.startswith('@'):
+        text = text[1:]
+    try:
+        instaloader.Instaloader().download_profile(text, profile_pic_only=True)
+    except (QueryReturnedNotFoundException, ProfileNotExistsException):
+        await status.delete()
+        await msg.reply("No Such Instagram Account Exists.", quote=True)
+        return
+    files = os.listdir(text.lower())
+    for file in files:
+        if file.endswith(".jpg"):
+            caption = f"Profile Picture of [@{text}](https://instagram.com/{text})"
+            await msg.reply_photo(f"{text}/{file}", caption=caption)
+            await status.delete()
+    shutil.rmtree(text)
